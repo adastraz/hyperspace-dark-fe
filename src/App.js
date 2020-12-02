c108c021265773a0eb86ecd851f8d363bc4e199d
adastraz <tylergosox@gmail.com>
2020-12-02 13:33:09 -0500

    ha well you can join discord now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './styles/dashboard.css'
 import Header from './components/Header'
+import { Switch, Route } from 'react-router-dom'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path='/' component={Welcome} />
+      </Switch>
     </>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../styles/imgs/Hyperspace_logo-HD.png'
 import Valorant from '../styles/imgs/valorant-logo.png'
 import RL from '../styles/imgs/rl-logo.png'
-// import Helmet from './styles/imgs/Hyperspace_logo-Helmet-2.png'
+import Helmet from '../styles/imgs/Hyperspace_logo-Helmet-2.png'
 
 const Header = () => {
     return (
         <div className='header'>
             <div className='title'>
-                <img className='mainlogo' src={Logo} />
+                <Link to='/'><img className='mainlogo' src={Logo} /></Link>
                 <h1>Hyperspace Dark</h1>
             </div>
+            <div className='discord'>
+                <a href='https://discord.gg/dXUFew7Gvn' target="_blank"><img src={Helmet} className='helmet' /></a>
+                <p className='msgdisc'>Join our discord!</p>
+            </div>
             <div className='gamelogo'>
-                <img src={Valorant} className='logo' />
-                <img src={RL} className='logo' />
+                <Link to='/valorant' className='nav'><img src={Valorant} className='logo' /></Link>
+                <Link to='/rl' className='rlnav'><img src={RL} className='logo' /></Link>
             </div>
         </div>
     )
