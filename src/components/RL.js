2fa0d3e99773c83932e16952891f3f2d11d5d355
adastraz <tylergosox@gmail.com>
2021-03-04 12:36:17 -0500

    made playercard dynamic for rl players

--- src/components/RL.js
+++ src/components/RL.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { PlayerDiv } from '../styles/Players'
+import { PlayersDiv, Divider, Lineup, Lineup2 } from '../styles/Players'
 
 const RL = () => {
     return (
-        <PlayerDiv>
-            <h1 className='lineuptitle'>Dark</h1>
-            <div className='lineup'>
-                <Link to='/player/Othos' className='name nav'>Othos</Link>
-                <Link to='/player/skythegrandmaguy' className='name nav'>skythegrandmaguy</Link>
-                <Link to='/player/tragedy.' className='name nav'>tragedy.</Link>
-            </div>
-            <h1 className='lineuptitle'>Light</h1>
-            <div className='lineup'>
-                <Link to='/player/ZachLara' className='name nav'>ZachLara</Link>
-                <Link to='/player/DeLaMora' className='name nav'>DeLaMora</Link>
-                <Link to='/player/sciiar' className='name nav'>sciiar</Link>
-            </div>
-        </PlayerDiv>
+        <PlayersDiv>
+            <Divider>
+                <h1 className='lineuptitle'>Dark</h1>
+                <Lineup2>
+                    <Link to='/player/Othos' className='name nav'>Othos</Link>
+                    <Link to='/player/skythegrandmaguy' className='name nav'>skythegrandmaguy</Link>
+                    <Link to='/player/FrugalPandy' className='name nav'>FrugalPandy</Link>
+                </Lineup2>
+            </Divider>
+            <Divider>
+                <h1 className='lineuptitle'>Light</h1>
+                <Lineup2>
+                    <Link to='/player/Sly' className='name nav'>Sly</Link>
+                    <Link to='/player/DeLaMora' className='name nav'>DeLaMora</Link>
+                    <Link to='/player/sciiar' className='name nav'>sciiar</Link>
+                </Lineup2>
+            </Divider>
+        </PlayersDiv>
     )
 }
 
